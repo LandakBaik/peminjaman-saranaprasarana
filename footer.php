7dc9601e4b83c451015d2105eab02932907440ea
Lannn-Uwu <rolandarafa917@gmail.com>
2026-05-14 23:15:16 +0700

    tes

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="d-flex align-items-center justify-content-between small">
             <div class="text-muted">Copyright &copy; Pinjam JTI 2026</div>
             <div>
-                <a href="#" class="text-decoration-none text-primary">Ketentuan Pengguna</a>
+                <a href="index.php?page=ketentuan" class="text-decoration-none text-primary">Ketentuan Pengguna</a>
             </div>
         </div>
     </div>
